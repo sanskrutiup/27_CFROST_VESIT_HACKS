446be382f32364e2d97ac6204c9457cb10b12595
ShreeSamal <shree.samal1502@gmail.com>
2023-03-03 10:48:22 +0530

    made changes in ui

--- Farm2door/farmtodoor/urls.py
+++ Farm2door/farmtodoor/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cart', views.cart, name='cart'),
     path('contact', views.contact, name='contact'),
     path('checkout', views.checkout, name='checkout'),
+    path('paytm_payment', views.paytm_payment, name='paytm_payment'),
 ]
